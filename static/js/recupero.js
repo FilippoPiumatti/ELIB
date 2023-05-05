ea2d10676d5783e94da531c678983ec1a5f615fa
unknown <frezyforbusiness@gmail.com>
2023-05-05 13:06:29 +0200

    aggiornata la grafica del hai dimenticato la password

--- static/js/recupero.js
+++ static/js/recupero.js
@@ -12,7 +12,9 @@ $(() => {
         });
         mailTest.done(function (serverData) {
             serverData = JSON.parse(serverData);
-            console.log("file : " + serverData)
+            console.log("file : " + serverData);
+            $("#errorMessage").show().text
+            ("Email presente nel nostro database! ti invieremo una mail con un link per resettare la tua password, se hai problemi a trovare la mail controlla la casella spam!")
         });
     });
 });
